ce5356d7f92e664729e97ac9f2f11ad4fe97c6ff
chic-page-6502 <zacharytq@gmail.com>
2020-09-01 13:54:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,16 @@ class App < Sinatra::Base
     end
 
     post "/teams" do
-      
+      params["team"]["heroes"].each do |hero_data|
+        Hero.new(name: hero_data["name"], power: hero_data["power"], bio: hero_data["bio"])
+      end
+
+      @team_name = params["team"]["name"]
+      @team_motto = params["team"]["motto"]
+      @heroes = Hero.all
+
+
+      erb :team
     end
 
 
